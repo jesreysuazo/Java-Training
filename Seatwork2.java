c3b5b206f4b0b3a6b5813873a07a077a53333eb1
jesreysuazo <85920977+jesreysuazo@users.noreply.github.com>
2025-12-05 08:55:48 +0800

    Add files via upload

---
+++ Seatwork2.java
@@ -0,0 +1,10 @@
+package com.example.M2_Activity2;
+
+public class Seatwork2 {
+    public static void main(String args[]){
+        Whiteboard wb = new Whiteboard();
+
+        int sum = wb.addNumbers(6,2);
+        System.out.println("The total is: " + String.valueOf(sum));
+    }
+}
